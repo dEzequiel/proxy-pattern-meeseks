468eeba8310f95f5ca44e4920f47ced11c15588a
Ezequiel De La Rosa <e.delarosa@innovation.es>
2022-10-19 13:00:47 +0200

    Testing object properties

--- test/mrmeeseeks/mrmeeseeks.test.js
+++ test/mrmeeseeks/mrmeeseeks.test.js
@@ -54,8 +54,7 @@ describe("Shadowign of variables", () => {
 
 describe("Mock box.pressButton", () => {
     const BoxPressButtonMock = jest.fn()
-                                    .mockImplementation( dimension =>
-                                                        dimension.push(Object.create(meesek)))
+                                    .mockImplementation( dimension => dimension.push(Object.create(meesek)))
                                     .mockName('pressButtonMock')
 
 
@@ -70,6 +69,32 @@ describe("Mock box.pressButton", () => {
             "I'm Mr Meeseeks! Look at meeee!"
             );
 
-    expect(reality[0]).toMatchObject(box.createMrMeeseks());
+    expect(reality[0]).toMatchObject(meesek);
+})
 
+describe("Object properties testing", () => {
+    test("Implicit this is working from function constructor", () => {
+        expect(meesek.messageOnCreate).toEqual(expect.stringMatching("I'm Mr Meeseeks! Look at meeee!"));
+    })
+
+    test("speakOnRequest() message is random", () => {
+        const expected = /^Oooh\b|\ssireee!$|\sma'am!$/;
+        expect(meesek.speakOnRequest()).toMatch(expected)
+    })
+
+    test("makeRequest() should add a new object property on fly", () => {
+
+        const meesek = box.createMrMeeseks()
+
+        meesek.makeRequest("open", "Jerry's stupid mayonnaise jar");
+        expect(meesek).toHaveProperty('action')
+        expect(meesek.hasOwnProperty('action')).toBeTruthy() // If a prototype method creates a new property, its part from the object itself.
+        expect(meesek.action).toEqual(expect.stringMatching("open" + " " + "Jerry's stupid mayonnaise jar"));
+    })
+
+    test("if makeRequest() is not invoke it shouldn't add a new object property on fly", () => {
+        const newMeesek = box.createMrMeeseks()
+        expect(newMeesek).not.toHaveProperty('action')
+        expect(newMeesek.hasOwnProperty('action')).toBeFalsy()
+    })
 })
